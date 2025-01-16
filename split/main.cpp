9142dd3d2f67f1f75d7e6020b9381f270f6ff6c4
jhyun <jhha1996@naver.com>
2025-01-16 23:29:13 +0900

    split

--- split/main.cpp
+++ split/main.cpp
@@ -1,32 +1,3 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//
-//vector<string> split(string input, string delimiter){
-//    vector<string> ret;
-//    long long pos;
-//    string token = "";
-//
-//    while((pos=input.find(delimiter)) != string::npos){ // input에서 delimiter를 찾지 못할 때까지 반복
-//        token = input.substr(0, pos); // 처음부터 pos까지 추출
-//        ret.push_back(token); // 벡터에 저장
-//        input.erase(0, pos + delimiter.length()); // 추출된 문자열 + token 길이 삭제
-//    }
-//    ret.push_back(input);
-//
-//    return ret;
-//}
-//
-//
-//int main() {
-//    string s = "Hello My Name is Ha!";
-//    string d = " ";
-//    vector<string> a = split(s, d);
-//    for(string b : a) cout << b << "\n";
-//    return 0;
-//}
-
-
 #include <iostream>
 #include <vector>
 
@@ -34,20 +5,20 @@ using namespace std;
 
 vector<string> split(string input, string delimiter){
     vector<string> ret;
-    string token = "";
     int pos;
+    string token = "";
 
-    while((pos = input.find(delimiter)) != string::npos){
-        token = input.substr(0, pos);
-        ret.push_back(token);
-        input.erase(0, pos + delimiter.length());
+    while((pos = input.find(delimiter)) != string::npos){ // delimiter 기준으로 문자열 추출
+        token = input.substr(0, pos); // 처음부터 pos까지 token
+        ret.push_back(token); // 추출된 token을 ret 벡터에 삽입
+        input.erase(0, pos + delimiter.length()); // token + delimiter 부분 삭제
     }
-
+    ret.push_back(input); // 마지막 단어
     return ret;
 }
 
 int main() {
-    string a = "Hello My Name Is Ha";
+    string a = "Hello My Name Is Jhyun";
     string b = " ";
     vector<string> c = split(a,b);
     for(string d : c) cout << d << "\n";
